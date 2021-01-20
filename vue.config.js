bca4558aa2bcd67208f572cf2d0095b21a92279b
chenzhihua <chenzhihua@tobosoft.com.cn>
2021-01-20 14:04:52 +0800

    vue空

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  outputDir: 'dist',   //build输出目录
+  assetsDir: 'assets', //静态资源目录（js, css, img）
+  lintOnSave: false, //是否开启eslint
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        // 修改 title
+        args[0].title= 'Live'
+        return args
+      })
+  },
+  devServer: {
+    open: true, //是否自动弹出浏览器页面
+    host: "localhost",
+    port: '8081',
+    https: false,
+    hotOnly: false,
+    proxy: {
+        '/': {
+            target: 'https://www.lemon1234.com', //API服务器的地址
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': ''
+            }
+        }
+    }
+  }
+}
